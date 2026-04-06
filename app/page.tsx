8eac33498fa91b8f87075b6aca876e277ac62d51
alumugii <angelicaumilin@gmail.com>
2026-04-06 14:40:42 -0500

    changed bunny image

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
         <Intro></Intro>
         <Image src="/suaa.png" className="sua" width="150" height="150" alt="sua heart"></Image>
         <Image src="/wings.png" className="wings" width="350" height="100" alt="pink wings"></Image>
+        <Image src="/bunnyy.png" className="bunny" width="600" height="300" alt="pink bunny"></Image>
       </div>
       <RightBanner></RightBanner>
     </div>
-    <Image src="/bunnyy.png" className="bunny" width="600" height="300" alt="pink bunny"></Image>
     </>
   );
 }
